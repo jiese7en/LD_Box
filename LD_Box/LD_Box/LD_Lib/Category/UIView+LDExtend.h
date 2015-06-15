698c6e32f22c13aa0154e6d1b014706250b2d18b
Jay <jiese7en@163.com>
2015-06-15 16:18:07 +0800

    增加Device和view坐标

---
+++ LD_Box/LD_Box/LD_Lib/Category/UIView+LDExtend.h
@@ -0,0 +1,33 @@
+//
+//  UIView+LDExtend.h
+//  LD_Box
+//
+//  Created by Jay on 15/6/15.
+//  Copyright (c) 2015年 LD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (LDExtend)
+
+@property (nonatomic) CGFloat left;
+
+@property (nonatomic) CGFloat top;
+
+@property (nonatomic) CGFloat right;
+
+@property (nonatomic) CGFloat bottom;
+
+@property (nonatomic) CGFloat width;
+
+@property (nonatomic) CGFloat height;
+
+@property (nonatomic) CGFloat centerY;
+
+@property (nonatomic) CGFloat centerX;
+
+@property (assign, nonatomic) CGSize size;
+
+@property (assign, nonatomic) CGPoint origin;
+
+@end
--- LD_Box/LD_Box/LD_Lib/Unit/LDDefine/LDDevice.h
+++ LD_Box/LD_Box/LD_Lib/Unit/LDDefine/LDDevice.h
@@ -9,6 +9,10 @@
 #ifndef LD_Box_LDDevice_h
 #define LD_Box_LDDevice_h
 
+//屏幕的高度
+#define SCREEN_HEIGHT [[UIScreen mainScreen] bounds].size.height
+#define SCREEN_WIDTH  [[UIScreen mainScreen] bounds].size.width
+
 
 //当前的系统版本
 #define SYSTEM_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
@@ -19,5 +23,32 @@
 
 #define IS_LATER_IOS7 (SYSTEM_VERSION >= 7.0)
 #define IS_BEFORE_IOS7 (SYSTEM_VERSION <= 7.0)
+
+
+//设备的型号
+#define IS_IPHONE [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone
+#define IS_PAD [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad
+
+#define IS_IPHONE4 ([[UIScreen mainScreen] bounds].size.height == 480)
+#define IS_IPHONE5 ([[UIScreen mainScreen] bounds].size.height == 568)
+#define IS_IPHONE6 ([[UIScreen mainScreen] bounds].size.height == 667)
+#define IS_IPHONE6_PLUS ([[UIScreen mainScreen] bounds].size.height == 736)
+
+
+//判断设备室真机还是模拟器
+#if TARGET_OS_IPHONE
+/** iPhone Device */
+#endif
+
+#if TARGET_IPHONE_SIMULATOR
+/** iPhone Simulator */
+#endif
+
+
+//判断系统为64位还是32位
+#if __LP64__
+#else
+#endif
+
 
 #endif
